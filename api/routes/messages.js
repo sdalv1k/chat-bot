97b7bb5d102cf8f17b6dda6979751ee4d20ea5b4
sdalvik <sindredalvik@gmail.com>
2022-08-26 18:48:09 +0200

    Added json file for API options

--- api/routes/messages.js
+++ api/routes/messages.js
@@ -3,24 +3,9 @@ const express = require("express");
 const router = express.Router();
 
 const Pandorabot = require("pb-node");
-
-var options = {
-  url: 'https://api.pandorabots.com',
-  app_id: "una63b6ed9",
-  user_key: "db4c05c73f6f0ae138589b6a1c22235e",
-  botname: "yoda"
-};
+const options = require('../../options.json'); 
 
 var bot = new Pandorabot(options);
-
-// var talkParams = {
-//     input: "hello"
-// }
-
-// bot.talk(talkParams, function (err, res) {
-//     if (!err) 
-//       console.log(res.responses);
-// });
 
 router.get('/', (req, res)=>{
     res.send("Message backend is working");
@@ -29,10 +14,11 @@ router.get('/', (req, res)=>{
 router.post("/", async (req, res) => {
     console.log(req.body);
     bot.talk(req.body, function (err, message) {
-        if (!err){
-        console.log(message.responses);
-        res.json(message);
+        if (!err) {
+            console.log(message.responses);
+            res.json(message);
         }
+        else console.log(err);
     });
 });
 
